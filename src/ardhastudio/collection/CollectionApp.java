345b3b9dbb4036d516e7cd2b1f894eeb9d5bd518
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-14 21:54:06 +0700

    Collection

---
+++ src/ardhastudio/collection/CollectionApp.java
@@ -0,0 +1,32 @@
+package ardhastudio.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CollectionApp {
+    public static void main(String[] args) {
+        Collection<String> collection = new ArrayList<>();
+        collection.add("Yohan");
+        collection.add("Apriyandi");
+        collection.addAll(List.of("Ardha", "Studio"));
+
+        for (var value : collection){
+            System.out.println(value );
+        }
+
+        System.out.println("Remove");
+
+        collection.remove("Apriyandi");
+        collection.removeAll(List.of("Ardha", "Studio"));
+
+        for (var value : collection){
+            System.out.println(value );
+        }
+
+        System.out.println(
+                collection.contains(List.of("Yohan", "Apriyandi"))
+        );
+
+    }
+}
