f8da0040fd763b1da7c308a3a6f14c534a134082
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-19 22:16:15 +0700

    IdentityHashmap

---
+++ src/ardhastudio/collection/IdentityHashMapApp.java
@@ -0,0 +1,21 @@
+package ardhastudio.collection;
+
+import java.util.IdentityHashMap;
+import java.util.Map;
+
+public class IdentityHashMapApp {
+    public static void main(String[] args) {
+        Map<String, String> map = new IdentityHashMap<>();
+
+        String key1 = "name.first";
+
+        String name = "name";
+        String dot = ".";
+        String first = "first";
+
+        String key2 = name + dot + first;
+
+        System.out.println(key1.equals(key2));
+        System.out.println(key1 == key2 );
+    }
+}
